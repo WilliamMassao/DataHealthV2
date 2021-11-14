a3d5e6e72004487e2192d2beb67cccb6b5ef6e10
WilliamMassao <william.massao.ftt@gmail.com>
2021-11-14 15:00:34 -0300

    Implementação da Controller ListaMedicamentosPacienteController

---
+++ src/main/java/com/example/datahealthv2/controllers/ListaMedicamentosPacienteController.java
@@ -0,0 +1,31 @@
+package com.example.datahealthv2.controllers;
+
+import com.example.datahealthv2.conexao.DAO.usuario.MedicamentoDAO;
+import com.example.datahealthv2.model.Medicamento;
+import com.example.datahealthv2.model.UsuarioPaciente;
+import com.example.datahealthv2.model.UsuarioProfissional;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ListaMedicamentosPacienteController extends BaseController {
+
+    UsuarioPaciente paciente = new UsuarioPaciente();
+
+    @FXML
+    public void initialize() {
+        addOnchageScreenListener(new onChangeScreen() {
+            @Override
+            public void onScreenChanged(String newScreen, Object objectData) {
+                paciente = (UsuarioPaciente) objectData;
+            }
+        });
+    }
+
+}
